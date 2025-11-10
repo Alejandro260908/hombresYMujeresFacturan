4ab123a1179f17d9135f4ca080b3898739f6f9ec
Alejandro <l21440338@mochis.tecnm.mx>
2025-11-10 13:34:15 -0700

    Crear agregado

--- Components/Servicios/ServicioControlador.cs
+++ Components/Servicios/ServicioControlador.cs
@@ -12,11 +12,19 @@ namespace hombresYMujeresFacturan.Components.Servicios
         }
         public async Task AgregarFactura(factura nuevaFactura)
         {
+            nuevaFactura.identificador = await GenerarNuevoId();
             await _servicioFactura.AgregarFactura(nuevaFactura);
         }
+
+        public async Task<int> GenerarNuevoId()
+        {
+            var juego = await _servicioFactura.ObtenerFacturas();
+            return juego.Any() ? juego.Max(t => t.identificador) + 1 : 1;
+        }
+
         public async Task<List<factura>> ObtenerFacturas()
         {
             return await _servicioFactura.ObtenerFacturas();
-        }
+        }   
     }
 }
